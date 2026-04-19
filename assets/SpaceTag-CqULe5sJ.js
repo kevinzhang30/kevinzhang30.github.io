7fdf8fd2f360228a52955584a9211d08b9cde8c2
kevinzhang30 <kevinzhang30@users.noreply.github.com>
2026-04-19 16:15:58 +0000

    deploy: f912bf6564e58b87f3164c031924c792bd57fd73

---
+++ assets/SpaceTag-CqULe5sJ.js
@@ -0,0 +1,1 @@
+import{j as t}from"./r3f-A9HPvB7I.js";function a({label:e}){return t.jsx("span",{className:"inline-block rounded-full border border-teal-500/20 bg-teal-500/10 px-2.5 py-0.5 text-xs font-medium text-teal-400",children:e})}export{a as S};
